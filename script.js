ed7b191d8010a2ff00028d3a51d4bf766f74f6be
weeezik <mkihanya.33@gmail.com>
2024-02-21 15:57:05 +0000

    build user input func and computer input func

--- script.js
+++ script.js
@@ -1,3 +1,55 @@
-console.log("Hello world!");
+// console.log("Hello world!");
+
+function getComputerChoice (){
+    let computerSelection = "";
+    let computerNumber = Math.floor(Math.random()*3);
+    let result = computerNumber;
+    if (result===0) {
+        computerSelection = "rock"
+    } else if (result===1) {
+        computerSelection = "paper"
+    } else if (result===2) {
+        computerSelection = "scissors"
+    }
+    console.log(computerSelection);
+    return computerSelection;
+}
+
+function getUserChoice (){
+   playerSelection1 = prompt("Type your throw! Rock, paper, or scissors?");
+    //remove spaces
+    let playerSelection2 = playerSelection1.replace(" ", "");
+    //make all lower case. pretty sure the function is string.toLowerCase()
+    let playerSelection = playerSelection2.toLowerCase();
+   console.log(playerSelection);
+   return playerSelection;
+}
+
+
+getUserChoice();
+getComputerChoice();
+
+
+function playRound (playerSelection, computerSelection){
+    
+
+    let playerSelection = "rock";
+    let computerSelection = getUserChoice();
+    console.log(playRound(playerSelection, computerSelection));
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
